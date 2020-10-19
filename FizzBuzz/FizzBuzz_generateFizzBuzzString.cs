21b3bf75efec6d5e4b6e2a7432bbcba1cc93623d
lenovo 3 <lenovo 3>
2020-10-19 17:27:09 +0200

    refacto : inline if else isFizz()

--- FizzBuzz/FizzBuzz_generateFizzBuzzString.cs
+++ FizzBuzz/FizzBuzz_generateFizzBuzzString.cs
@@ -6,21 +6,18 @@ namespace FizzBuzz
     {
         public string generateFizzBuzzString (int [] arrayOfNumbers)
         {
-            var fizzBuzzString ="";
+            
             var firstNumber = arrayOfNumbers[0];
 
-            isFizz(firstNumber) ? "fizz" :  Convert.ToString(firstNumber) ;
+            return isFizz(firstNumber) ? "fizz" :  Convert.ToString(firstNumber) ;
 
             throw new NotImplementedException ("You have to write a test first");
         }
 
         public static bool isFizz(int number)
         {
-            if (number % 3 == 0)
-            {
-                return true;
-            }
-            return false;
+            return number % 3 == 0 ? true : false;
+        
         }
     }
 }
