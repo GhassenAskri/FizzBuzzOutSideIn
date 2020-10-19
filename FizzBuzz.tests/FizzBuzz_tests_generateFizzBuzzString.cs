e023d5c634ba04dd5a979be33a6bfdfa73820fec
lenovo 3 <lenovo 3>
2020-10-19 16:15:31 +0200

    Adding a test for multiples of three

--- FizzBuzz.tests/FizzBuzz_tests_generateFizzBuzzString.cs
+++ FizzBuzz.tests/FizzBuzz_tests_generateFizzBuzzString.cs
@@ -11,6 +11,7 @@ namespace FizzBuzz.tests
         {
             _fizzBuzz = new FizzBuzz_generateFizzBuzzString();
         }
+        
 
         [Description("When I use array of numbers equal to [1,9,10,2,0,4,15,10],"+ 
                     "I get a fizzbuzz string equal to 1 fizz buzz 2 0 4 fizzbuzz buzz")]
@@ -24,6 +25,9 @@ namespace FizzBuzz.tests
             //Then
             Assert.AreEqual(fizzBuzzString,"1 fizz buzz 2 0 4 fizzbuzz buzz");
         }
+
+
+
 
         [Description("When I use array of numbers that not multiple of 3 or 5,"+ 
                     "I get the same number as string ")]
@@ -47,5 +51,31 @@ namespace FizzBuzz.tests
             //Then
             Assert.AreEqual(fizzBuzzString,"4");
         }
+        
+        
+        
+        [Description("When I use array of numbers that are multiple of 3,"+ 
+                    "I get fizz")]
+        [TestMethod]
+        public void generateFizzBuzzStringTestForOneNumberMultipleOfThreeEqualTo9()
+        {
+            //Given
+            int [] arrayOfNumbers = new int [] {9};
+            //When
+            var fizzBuzzString = _fizzBuzz.generateFizzBuzzString(arrayOfNumbers);
+            //Then
+            Assert.AreEqual(fizzBuzzString,"fizz");
+        }
+        [TestMethod]
+        public void generateFizzBuzzStringTestForOneNumberMultipleOfThreeEqualTo3()
+        {
+            //Given
+            int [] arrayOfNumbers = new int [] {3};
+            //When
+            var fizzBuzzString = _fizzBuzz.generateFizzBuzzString(arrayOfNumbers);
+            //Then
+            Assert.AreEqual(fizzBuzzString,"fizz");
+        }
+
     }
 }
--- FizzBuzz/FizzBuzz_generateFizzBuzzString.cs
+++ FizzBuzz/FizzBuzz_generateFizzBuzzString.cs
@@ -6,8 +6,33 @@ namespace FizzBuzz
     {
         public string generateFizzBuzzString (int [] arrayOfNumbers)
         {
-            return Convert.ToString(arrayOfNumbers[0]);
+            var fizzBuzzString ="";
+            var FIRST_NUMBER = arrayOfNumbers[0];
+
+
+            if(isFizz(FIRST_NUMBER)){
+                 fizzBuzzString ="fizz";
+            }
+            else{
+                fizzBuzzString = convertToString(FIRST_NUMBER);
+            }
+             
+            return fizzBuzzString;
+
             throw new NotImplementedException ("You have to write a test first");
+        }
+
+        public static string convertToString (int number)
+        {
+            return Convert.ToString(number);
+        }
+        public static bool isFizz(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
