314dbac0fa5753f970e927635bca14f0adc6a891
lenovo 3 <lenovo 3>
2020-10-19 09:48:36 +0200

    Create FizzBuzz class library

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        public string generateFizzBuzzString (int [] arrayOfNumbers)
+        {
+            throw new NotImplementedException ("You have to write a test first");
+        }
+    }
+}
